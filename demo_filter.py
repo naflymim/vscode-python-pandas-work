62e45f267f499241e40420588102b7a463c881d7
Nafly Mohammed <nafly.it@gmail.com>
2018-09-14 20:49:43 +0530

    Path correction

--- demo_filter.py
+++ demo_filter.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-df = pd.read_pickle(r"I:\Web Developments\Python\VSCode\Panadas\Demo - Json\TateGallery\artwork_data_mod.zip", "zip")
+df = pd.read_pickle(r"I:\Web Developments\Python\VSCode\Panadas\Demo - Json\TateGallery\artwork_data_mod")
 
 artistFilter = df["artist"] == 'Bacon, Francis'
 print(artistFilter.value_counts())
